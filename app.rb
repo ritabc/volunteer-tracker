6d1b4a17f9f2aad1f951ee967f366235b63ab281
ritabc <ritabc@gmail.com>
2018-07-13 11:26:11 -0700

    Add index.erb file and first set of routes for functionality of home page and adding a project

---
+++ app.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require("sinatra/reloader")
+also_reload('lib/**/*.rb')
+require('./lib/project')
+require('./lib/volunteer')
+require('pg')
+require('pry')
+
+DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/') do
+  title = params.fetch('title')
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:index)
+end
