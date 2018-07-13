28bdaab05003013bf270f4cd263d1af73357171d
ritabc <ritabc@gmail.com>
2018-07-13 11:02:02 -0700

    Add project.delete method and pass corresponding spec

--- lib/project.rb
+++ lib/project.rb
@@ -55,4 +55,8 @@ class Project
       DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
     end
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id};") 
+  end
 end
